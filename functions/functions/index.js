f914d5abeb072b2fb3ebaea97b0143fa96a12420
Pierre Leroux <panacpp@gmail.com>
2021-10-06 16:48:07 +0200

    Update index.js

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -329,7 +329,7 @@ app.handle("ask_to_resume_listening_at_last_offset", async (conv) => {
     const date = history.d;
     // TODO: use the date info
     
-    conv.add("Voulez-vous continuez la lecture ?");
+    conv.add("Voulez-vous reprendre la lecture là où c'était arrêtée ?");
   } else {
     console.log("no need to ask to resume");
     conv.scene.next.name = "player";
