2f2b83804dcc8bf3ac89ad579055591ec6a85246
Pierre Leroux <panacpp@gmail.com>
2021-11-03 13:33:57 +0100

    webhooks fix test player

--- webhooks/functions/index.js
+++ webhooks/functions/index.js
@@ -74,6 +74,49 @@ async function getPubsFromFeed(url) {
   return list;
 }
 
+//////////
+// TEST //
+//////////
+
+app.handle("test_player_sdk", (conv) => {
+
+  const nb = conv.intent.params.number.resolved;
+
+  switch (nb) {
+
+    case 123:
+
+      conv.user.params = {
+        ...conv.user.params,
+        p_i: 2,
+        p_n: "therese_raquin_emile_zola.json",
+        p_t: 123,
+        player: {
+          ...conv.user.params.player || {},
+          ["therese_raquin_emile_zola.json"]: {
+            p_i: 2,
+            p_n: "therese_raquin_emile_zola.json",
+            p_t: 123,
+          },
+        },
+      };
+
+      break;
+
+    case 456:
+
+      break;
+    case 789:
+
+      break;
+  }
+
+  console.log("test_PLAYER");
+  console.log(conv.user.params);
+
+  conv.add(`test player ${nb}`);
+});
+
 app.handle("setup_test_sdk", (conv) => {
 
   const nb = conv.intent.params.number.resolved;
@@ -85,6 +128,10 @@ app.handle("setup_test_sdk", (conv) => {
   conv.add(`setup test ${nb}`);
 
 });
+
+//////////
+// TEST //
+//////////
 
 // ----------------
 //
