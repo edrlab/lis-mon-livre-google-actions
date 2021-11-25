c8f7d74bec864705816c0e519c1a4935f2663adb
Pierre Leroux <panacpp@gmail.com>
2021-11-25 18:27:05 +0100

    fix

--- webhooks/functions/src/translation/index.ts
+++ webhooks/functions/src/translation/index.ts
@@ -1,6 +1,7 @@
 import i18next from 'i18next';
 import * as frTranslation from './fr/fr.json';
 import {Translations} from './../typings/i18n';
+import { DEFAULT_LANGUAGE } from '../constants';
 
 i18next.init({
   resources: {
@@ -8,6 +9,7 @@ i18next.init({
       translation: frTranslation,
     },
   },
+  fallbackLng: DEFAULT_LANGUAGE,
 });
 
 export type TI18nKey = Translations['keys']['fr'];
@@ -16,3 +18,4 @@ export const i18n = i18next;
 
 export const t = i18n.t as (key: TI18nKey, options? : object) => any;
 
+i18n.t = t as any;
