634d16fa870cffdfee127a9357fdb902dcf6ca16
Pierre Leroux <panacpp@gmail.com>
2021-11-25 19:04:26 +0100

    fix: constant

--- test/dev/test/constant.ts
+++ test/dev/test/constant.ts
@@ -4,7 +4,7 @@ import {ok} from 'assert';
 
 export const DEFAULT_LOCALE = 'fr-FR';
 export const DEFAULT_SURFACE = 'PHONE';
-export const HOME_PROMPT =  "Bienvenue dans l'application d'écoute de livre audio valentin hauy";
+export const HOME_PROMPT =  "Bienvenue dans l'application d'écoute de livre audio valentin audio";
 export const MEMBER_PROMPT = "Bienvenue dans l'espace membres. Les commandes possibles sont: sélection, lecture, recherche. Que voulez-vous faire ?";
 
 export const PROJECT_ID = env['PROJECT_ID'] || '';
