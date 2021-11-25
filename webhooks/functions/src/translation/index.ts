7522be41fbe3282559a39f731f3bc46a67967e91
Pierre Leroux <panacpp@gmail.com>
2021-11-25 18:44:27 +0100

    fix: i18n'

--- webhooks/functions/src/translation/index.ts
+++ webhooks/functions/src/translation/index.ts
@@ -1,4 +1,4 @@
-import i18next from 'i18next';
+import * as i18next from 'i18next';
 import * as frTranslation from './fr/fr.json';
 import {Translations} from './../typings/i18n';
 import {DEFAULT_LANGUAGE} from '../constants';
@@ -17,5 +17,3 @@ export type TI18nKey = Translations['keys']['fr'];
 export const i18n = i18next;
 
 export const t = i18n.t as (key: TI18nKey, options? : object) => any;
-
-i18n.t = t as any;
