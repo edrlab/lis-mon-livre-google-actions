d22de54260082c337f296a2da7813fe0b695ac1d
Pierre Leroux <panacpp@gmail.com>
2022-02-14 20:12:01 +0100

    fix: global cancel, fallback and silence

--- webhooks/functions/src/controller/handler/index.ts
+++ webhooks/functions/src/controller/handler/index.ts
@@ -11,7 +11,33 @@ export const handler = (app = new Assistant({})) => {
 
   app.handle('main', main);
 
-  app.handle('cancel', () => {});
+  app.handle('cancel', (m) => {
+    m.say('bye.1');
+  });
+
+  app.handle('fallback_1', (m) => {
+    m.say('fallback.1');
+  });
+
+  app.handle('fallback_2', (m) => {
+    m.say('fallback.2');
+  });
+
+  app.handle('fallback_end', (m) => {
+    m.say('bye.1'); 
+  });
+
+  app.handle('silence_1', (m) => {
+    m.say('silence.1');
+  });
+
+  app.handle('silence_2', (m) => {
+    m.say('silence.2');
+  });
+
+  app.handle('silence_end', (m) => {
+    m.say('bye.1'); 
+  });
 
   home_user(app);
   home_new_user(app);
