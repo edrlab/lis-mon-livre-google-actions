50503e4e2babd91f2cb94746bb1d5fe92fb1ac0c
Pierre Leroux <panacpp@gmail.com>
2022-02-14 16:34:38 +0100

    feat: selection first step

--- webhooks/functions/src/controller/handler/index.ts
+++ webhooks/functions/src/controller/handler/index.ts
@@ -5,6 +5,7 @@ import { home_new_user_no } from "./home_new_user_no";
 import { home_user } from "./home_user";
 import { info } from "./info";
 import { main } from "./main";
+import { selection } from "./selection";
 
 export const handler = (app = new Assistant({})) => {
 
@@ -17,4 +18,5 @@ export const handler = (app = new Assistant({})) => {
   home_new_user_no(app);
   home_new_user_maybe_later(app);
   info(app);
+  selection(app);
 }
--- webhooks/functions/src/typings/sdkScene.d.ts
+++ webhooks/functions/src/typings/sdkScene.d.ts
@@ -1,4 +1,4 @@
 // npm run scene-typed ;
 
 
-export type TSdkScene = 'bookshelf' | 'collections' | 'home_new_user' | 'home_new_user_AccountLinking' | 'home_new_user_maybe_later' | 'home_new_user_no' | 'home_user' | 'info' | 'search';
+export type TSdkScene = 'bookshelf' | 'collections' | 'home_new_user' | 'home_new_user_AccountLinking' | 'home_new_user_maybe_later' | 'home_new_user_no' | 'home_user' | 'info' | 'search' | 'selection';
