78cdc90ea1a6f11e7ad659d241ca1586be3577a1
Pierre Leroux <panacpp@gmail.com>
2022-03-09 14:42:20 +0100

    replace edrlab-1 with cela-2

--- webhooks/functions/src/constants.ts
+++ webhooks/functions/src/constants.ts
@@ -17,7 +17,7 @@ export const PADDING_GROUP = 5;
 export const PADDING_PUB = 3;
 export const LAST_SEEN_THRESHOLD = 72;
 
-export const PROJECT_ID = 'edrlab-1';
+export const PROJECT_ID = 'cela-2';
 
 
 type TLang = 'fr' | 'en';
