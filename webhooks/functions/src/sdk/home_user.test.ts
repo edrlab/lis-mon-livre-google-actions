d34e2c9dd082a891ccde3685a5955e8efdccbb52
Pierre Leroux <panacpp@gmail.com>
2022-03-21 11:31:47 +0100

    fixes #51: home_user new_playing missing a translation

--- webhooks/functions/src/sdk/home_user.test.ts
+++ webhooks/functions/src/sdk/home_user.test.ts
@@ -136,7 +136,8 @@ describe('home_user handler', () => {
 
       data.prompt.firstSimple.speech.should.to.be.eq('Last time, you read Chapter 10 of my title, hello, which you can resume where you left off.\n' +
       'You can search for a new one alltogether.\n' +
-      'You can search for a book by title or author, browse our collections or check your bookshelf to start reading one of your preselected books.\n');
+      'You can search for a book by title or author, browse our collections or check your bookshelf to start reading one of your preselected books.\n' +
+      'What would you like to do?\n');
     });
     it('on enter with a current playing and history', async () => {
       body.handler.name = 'home_user__on_enter';
@@ -175,7 +176,8 @@ describe('home_user handler', () => {
       data.prompt.firstSimple.speech.should.to.be.eq('Last time, you read Chapter 10 of my title, hello, which you can resume where you left off.\n' +
       'You are also reading 3 other recent books, which you can choose from.\n' +
       'You can search for a new one alltogether.\n' +
-      'You can search for a book by title or author, browse our collections or check your bookshelf to start reading one of your preselected books.\n');
+      'You can search for a book by title or author, browse our collections or check your bookshelf to start reading one of your preselected books.\n' +
+      'What would you like to do?\n');
     });
 
     it('repeat', async () => {
