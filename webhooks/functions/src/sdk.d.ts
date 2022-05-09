89fba3a58240c234c7643cb638944caf3569a99a
Pierre Leroux <panacpp@gmail.com>
2022-05-09 17:41:10 +0200

    clean: rm -rf avh-prod branch

--- webhooks/functions/src/sdk.d.ts
+++
@@ -1,4 +0,0 @@
-// npm run scene-typed ;
-
-
-export type TSdkScene =  'ask_to_resume_listening_at_last_offset' | 'home_lvl1' | 'home_lvl1_AccountLinking' | 'home_members_lvl2' | 'player' | 'search' | 'select_group' | 'select_publication' | 'selection_lvl3';
