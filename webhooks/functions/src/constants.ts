3206ab4c741c478f010232053f2d665ddec29523
Pierre Leroux <panacpp@gmail.com>
2022-08-17 19:22:18 +0200

    avh-api: fix the per_page_collection to 5 in genre and thematique preselections list

--- webhooks/functions/src/constants.ts
+++ webhooks/functions/src/constants.ts
@@ -6,8 +6,8 @@ export const EDRLAB_FUNCTION_URL = "";
 export const BOOKSHELF_URL = 'https://eole-recette.avh.asso.fr/api/v1/my_list?per_page=5';
 export const SEARCH_URL = 'https://eole-recette.avh.asso.fr/api/v1/search?search={query}&support=1&per_page=5';
 export const SEARCH_URL_FN = (value: string) => SEARCH_URL.replace("{query}", value);
-export const THEMATIC_LIST_URL = 'https://eole-recette.avh.asso.fr/api/v1/preselections/thematique?per_page=5';
-export const GENRE_LIST_URL = 'https://eole-recette.avh.asso.fr/api/v1/preselections/genre?per_page=5';
+export const THEMATIC_LIST_URL = 'https://eole-recette.avh.asso.fr/api/v1/preselections/thematique?per_page=5&per_page_collection=5';
+export const GENRE_LIST_URL = 'https://eole-recette.avh.asso.fr/api/v1/preselections/genre?per_page=5&per_page_collection=5';
 export const DEFAULT_LANGUAGE: TDefaultLanguage = 'fr';
 
 export type TDefaultLanguage = Extract<TLang, 'fr'>;
